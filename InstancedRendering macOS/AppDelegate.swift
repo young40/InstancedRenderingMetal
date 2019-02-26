e3a1cc99a90c2f60963ffd5a28aaa546b5da99bd
Peter Young <young40@gmail.com>
2019-02-26 23:19:15 +0800

    Initial Commit

---
+++ InstancedRendering macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  InstancedRendering macOS
+//
+//  Created by 杨世玲 on 2019/2/26.
+//  Copyright © 2019 杨世玲. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
